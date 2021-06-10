e612643879f7cd5562eba93863b1e3e40c40ca03
SydneyDF <maximumsupernova@gmail.com>
2021-06-10 10:16:19 +0800

    attempting to solve minor merge

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
 define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
 define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
 define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-define('RDS_PORT', $_SERVER['RDS_PORT']);
+
 
 return [
 
@@ -53,7 +53,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST',RDS_HOSTNAME),
-            'port' => env('DB_PORT', RDS_PORT),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE',RDS_DB_NAME),
             'username' => env('DB_USERNAME',RDS_USERNAME),
             'password' => env('DB_PASSWORD', RDS_PASSWORD),
