ab4fc217b8df1f7fde9bd48b6cde0cb43c1dae8a
SydneyDF <maximumsupernova@gmail.com>
2021-06-10 10:11:36 +0800

    pushed minor database.php env pwd change

--- config/database.php
+++ config/database.php
@@ -6,6 +6,7 @@ define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
 define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
 define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
 define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+define('RDS_PORT', $_SERVER['RDS_PORT']);
 
 return [
 
@@ -52,10 +53,10 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST',RDS_HOSTNAME),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', RDS_PORT),
             'database' => env('DB_DATABASE',RDS_DB_NAME),
             'username' => env('DB_USERNAME',RDS_USERNAME),
-            'password' => RDS_PASSWORD,
+            'password' => env('DB_PASSWORD', RDS_PASSWORD),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
