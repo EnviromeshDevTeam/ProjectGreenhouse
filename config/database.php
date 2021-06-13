3284b872c2e942aa3993eaa5d92c8d355b560888
SydneyDF <maximumsupernova@gmail.com>
2021-06-13 22:14:29 +0800

    insanity v3

--- config/database.php
+++ config/database.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Str;
 
-define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-define('RDS_DB_PORT', $_SERVER['RDS_PORT']);
-
-
 return [
 
     /*
@@ -53,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST',RDS_HOSTNAME),
-            'port' => env('DB_PORT', RDS_DB_PORT),
-            'database' => env('DB_DATABASE',RDS_DB_NAME),
-            'username' => env('DB_USERNAME',RDS_USERNAME),
-            'password' => env('DB_PASSWORD',RDS_PASSWORD),
+            'host' => env('RDS_HOSTNAME','DB_HOST'),
+            'port' => env('RDS_DB_PORT', 'DB_PORT'),
+            'database' => env('RDS_DB_NAME','DB_DATABASE'),
+            'username' => env('RDS_USERNAME', 'DB_USERNAME'),
+            'password' => env('RDS_PASSWORD','DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
