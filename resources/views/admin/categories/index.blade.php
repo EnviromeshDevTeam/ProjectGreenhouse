ad5346e134c18a1da0777ba368eb6993b3a590b8
SydneyDF <maximumsupernova@gmail.com>
2021-06-18 12:58:22 +0800

    Added bootstrap css to device/cat tables

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Category Data') }}
         </h2>
+        <a class="btn btn-primary" href="{{route('categories.create')}}">Create New Category</a>
     </x-slot>
 
     <div class="py-12">
@@ -10,56 +11,54 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <!--Your View Table here-->
 
-                <form action="{{route('categories.create')}}">
-                    @method('GET')
-                    @csrf
-                    <button class="button button-primary" type="submit">CREATE NEW CATEGORY</button>
-                </form>
-
                 <!--div.mt-8.mx-auto>table.w-100>tr>(th*4)Press TAB after the bracket to get a basic table with 4 headers -->
-                <div class="mt-8 mx-auto">
-                    <table class="w-100">
+
+                <table class="table table-bordered table-striped table-hover table-sm">
+                    <thead class="thead-dark">
                         <tr>
                             <th>Id</th>
-                            <th>Name</th>
+                            <th>Dataname</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
                         </tr>
+                    </thead>
 
-                        @forelse($categories as $category)
-                            <tr>
-                                <td class="text-center">{{$category->id}}</td>
-                                <td class="text-center">{{$category->name}}</td>
+                    @forelse($categories as $category)
+                        <tr>
+                            <td class="text-center">{{$category->id}}</td>
+                            <td class="text-center">{{$category->dataname}}</td>
 
-                                <td class="flex justify-center">
+                            <td>
+                                <form action="{{route('categories.show', $category)}}">
+                                    @csrf
+                                    @method('GET')
+                                    <button class="w-100 btn btn-success" type="submit">VIEW</button>
+                                </form>
+                            </td>
 
-                                    <!--Show func for specific device-->
+                            <td>
+                                <form action="{{route('categories.edit', $category)}}">
+                                    @csrf
+                                    @method('GET')
+                                    <button class="w-100 btn btn-warning" type="submit">UPDATE</button>
+                                </form>
+                            </td>
+                            <td>
 
-                                    <form action="{{route('categories.show', $category)}}">
-                                        @csrf
-                                        @method('GET')
-                                        <button class="button button-primary" type="submit">VIEW</button>
-                                    </form>
-
-                                    <form action="{{route('categories.edit', $category)}}">
-                                        @csrf
-                                        @method('GET')
-                                        <button class="button button-primary" type="submit">UPDATE</button>
-                                    </form>
-
-                                    <!--Delete func for specific device-->
-                                    <form method="POST" action="{{route('categories.destroy', $category)}}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="button button-red" type="submit">DELETE</button>
-                                    </form>
-
-                                    <!--If Array empty-->
-                                    @empty
-                                        <p>'No Devices found!' <a href="{{route('categories.create')}}">Click here to add a new Category</a></p>
-                                    @endforelse
-                                </td>
-                            </tr>
-                    </table>
-                </div>
+                                <!--Delete func for specific device-->
+                                <form method="POST" action="{{route('categories.destroy', $category)}}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="w-100 btn btn-danger" type="submit">DELETE</button>
+                                </form>
+                            </td>
+                            @empty
+                                <p>'No Devices found!' <a href="{{route('categories.create')}}">Click here to add a
+                                        new Category</a></p>
+                            @endforelse
+                        </tr>
+                </table>
             </div>
         </div>
     </div>
--- resources/views/admin/devices/index.blade.php
+++ resources/views/admin/devices/index.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Device Data') }}
         </h2>
+        <a class="btn btn-primary" href="{{route('devices.create')}}">Create New Device</a>
     </x-slot>
 
     <div class="py-12">
@@ -10,62 +11,63 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <!--Your View Table here-->
 
-                <form action="{{route('devices.create')}}">
-                    @method('GET')
-                    @csrf
-                    <button class="button button-primary" type="submit">CREATE NEW DEVICE</button>
-                </form>
-
                 <!--div.mt-8.mx-auto>table.w-100>tr>(th*4)Press TAB after the bracket to get a basic table with 4 headers -->
-                <div class="mt-8 mx-auto">
-                    <table class="w-100 table-auto">
+                <table class="table table-bordered table-striped table-hover table-sm">
+                    <thead class="thead-dark">
                         <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Address</th>
-                            <th>Updated_At</th>
-                            <th>Created_At</th>
+                            <th class="text-center">Id</th>
+                            <th class="text-center">Name</th>
+                            <th class="text-center">Address</th>
+                            <th class="text-center">Updated_At</th>
+                            <th class="text-center">Created_At</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
                         </tr>
+                    </thead>
 
-                        @forelse($devices as $device)
-                            <tr>
-                                <td class="text-center">{{$device->id}}</td>
-                                <td class="text-center">{{$device->name}}</td>
-                                <td class="text-center">{{$device->address}}</td>
-                                <td class="text-center">{{$device->updated_at}}</td>
-                                <td class="text-center">{{$device->created_at}}</td>
+                    @forelse($devices as $device)
+                        <tr>
+                            <td class="text-center">{{$device->id}}</td>
+                            <td class="text-center">{{$device->name}}</td>
+                            <td class="text-center">{{$device->address}}</td>
+                            <td class="text-center">{{$device->updated_at}}</td>
+                            <td class="text-center">{{$device->created_at}}</td>
 
-                                <td class="flex justify-center">
+                            <td>
+                                <!--Show func for specific device-->
 
-                                    <!--Show func for specific device-->
+                                <form action="{{route('devices.show', $device)}}">
+                                    @method('GET')
+                                    @csrf
+                                    <button class="w-100 btn btn-success" type="submit">VIEW</button>
+                                </form>
 
-                                    <form action="{{route('devices.show', $device)}}">
-                                        @method('GET')
-                                        @csrf
-                                        <button class="button button-primary" type="submit">VIEW</button>
-                                    </form>
+                            </td>
+                            <td>
+                                <form action="{{route('devices.edit', $device)}}">
+                                    @method('GET')
+                                    @csrf
+                                    <button class="w-100 btn btn-warning" type="submit">UPDATE</button>
+                                </form>
+                            </td>
+                            <td>
+                                <!--Delete func for specific device-->
+                                <form method="POST" action="{{route('devices.destroy', $device)}}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="w-100 btn btn-danger" type="submit">DELETE</button>
+                                </form>
+                            </td>
 
-                                    <form action="{{route('devices.edit', $device)}}">
-                                        @method('GET')
-                                        @csrf
-                                        <button class="button button-primary" type="submit">UPDATE</button>
-                                    </form>
+                            <!--If Array empty-->
+                            @empty
+                                <p>'No Devices found!' <a href="{{route('devices.create')}}">Click here to add a new
+                                        Device</a></p>
+                            @endforelse
 
-                                    <!--Delete func for specific device-->
-                                    <form method="POST" action="{{route('devices.destroy', $device)}}">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button class="button button-red" type="submit">DELETE</button>
-                                    </form>
-
-                                    <!--If Array empty-->
-                                    @empty
-                                        <p>'No Devices found!' <a href="{{route('devices.create')}}">Click here to add a new Device</a></p>
-                                    @endforelse
-                                </td>
-                            </tr>
-                    </table>
-                </div>
+                        </tr>
+                </table>
             </div>
         </div>
     </div>
