47b844a318cf32214c8c2fbf5461c5e7a695aba7
Alex <47835666+Alco4998@users.noreply.github.com>
2021-06-29 10:24:20 +0800

    reload database.php

--- config/database.php
+++ config/database.php
@@ -43,16 +43,14 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-
-        //To get rid of hardcoded errors ive just added ENV KEYS RDS... to AWS Instance ENV
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST','RDS_HOSTNAME'), //testing -sdf
-            'port' => env('DB_PORT', 'RDS_DB_PORT'),
-            'database' => env('DB_DATABASE','RDS_DB_NAME'),
-            'username' => env('DB_USERNAME','RDS_USERNAME'),
-            'password' => env('DB_PASSWORD','RDS_PASSWORD'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
